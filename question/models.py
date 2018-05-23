a45bcbfee9ce75bfa1d7b0789a298f49155b5739
Marcus Broberg <broberg.marcus@gmail.com>
2018-05-23 21:05:46 +0200

    Added votes and fixed relationsships

--- question/models.py
+++ question/models.py
@@ -1,6 +1,6 @@
 from app import db
 from sqlalchemy.dialects.postgresql import JSON
-
+from datetime import datetime
 
 class User(db.Model):
     __tablename__ = 'users'
@@ -8,7 +8,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String())
     password = db.Column(db.String())
-
+    questions = db.relationship('Question', backref='user', lazy=True)
     def __init__(self, username, password):
         self.username = username
         self.password = password
@@ -21,9 +21,24 @@ class Question(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String())
-
+    # addresses = db.relationship('Address', backref='person', lazy=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    votes = db.relationship('Vote', backref='question', lazy=True)
     def __init__(self, text):
         self.text = text
 
     def __repr__(self):
         return '<id {}>'.format(self.id)
+
+class Vote(db.Model):
+    __tablename__ = 'votes'
+
+    id = db.Column(db.Integer, primary_key=True)
+    created_date = db.Column(db.DateTime, nullable=False,
+        default=datetime.utcnow)
+    question_id = db.Column(db.Integer, db.ForeignKey('questions.id'))
+    def __init__(self):
+        pass
+
+    def __repr__(self):
+        return '<id {}, question {}'.format(self.id, self.question_id)
