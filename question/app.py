5ab1c42a0ef039115c6eb61af47677ad54f9f058
Marcus Broberg <broberg.marcus@gmail.com>
2018-05-25 23:03:35 +0200

    adds toolbar to the app

--- question/app.py
+++ question/app.py
@@ -2,10 +2,11 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from questions import bp
-
+from flask_debugtoolbar import DebugToolbarExtension
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+toolbar = DebugToolbarExtension(app)
 #db = SQLAlchemy(app)
 
 app.register_blueprint(bp, url_prefix='/q')
