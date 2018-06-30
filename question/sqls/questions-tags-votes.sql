78005692b2c3cb54eddfaaa2b5f9c3d2af21a6bc
Marcus Broberg <broberg.marcus@gmail.com>
2018-06-30 09:29:40 +0200

    Started to work on stored procedure for questions

---
+++ question/sqls/questions-tags-votes.sql
@@ -0,0 +1,6 @@
+﻿SELECT questions.id, questions.text AS text, STRING_AGG(tags.text, ',') AS tags, count(votes.id) AS votes, count(question_tags.id) AS n_tags FROM questions 
+JOIN votes ON votes.question_id=questions.id
+LEFT JOIN question_tags on question_tags.question_id=questions.id
+LEFT JOIN tags on tags.id=question_tags.tag_id
+GROUP BY questions.id
+ORDER BY count(votes.id) DESC
