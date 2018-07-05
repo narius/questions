2e36d16ebc50c4767b4cfc4699e5bdf141c2bb0e
Marcus Broberg <broberg.marcus@gmail.com>
2018-07-05 15:24:06 +0200

    only counts distinct votes do avoid duobles

--- question/questions.py
+++ question/questions.py
@@ -26,7 +26,7 @@ def get_all_questions():
     app.logger.info('Get all questions')
     db = engine.connect()
     questions = db.execute(
-        "SELECT questions.id, questions.text AS text, STRING_AGG(DISTINCT tags.text, ',') AS tags_text, count(votes.id) AS votes FROM questions"
+        "SELECT questions.id, questions.text AS text, STRING_AGG(DISTINCT tags.text, ',') AS tags_text, count(DISTINCT votes.id) AS votes FROM questions"
         " INNER JOIN votes ON votes.question_id=questions.id"
         " LEFT JOIN question_tags on question_tags.question_id=questions.id"
         " LEFT JOIN tags on tags.id=question_tags.tag_id"
